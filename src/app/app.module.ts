0d7723579702f19148b0e85e1c138e3ba4e4e4a2
nirmayi <nirmayi987@gmail.com>
2021-06-10 21:44:50 +0530

    'second'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { HotelsComponent } from './hotels/hotels.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutusComponent,
+    HotelsComponent,
+    RestaurantsComponent,
+    LoginComponent,
+    RegisterComponent,
+    ContactusComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
